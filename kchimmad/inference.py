c63345762edd6babf43fc16a888ff529480bf82b
KedarnathKC <kedarnathchimmad555@gmail.com>
2024-11-11 03:22:06 +0000

    Inference Results with train as fewshot examples

--- kchimmad/inference.py
+++ kchimmad/inference.py
@@ -23,7 +23,7 @@ os.environ['TRANSFORMERS_CACHE'] = '../transformers_cache'
 
 # Loading data
 data_test = load_from_disk("../datasets/gsm8k/test/")
-# data_train = load_from_disk("../datasets/gsm8k/train/")
+data_train = load_from_disk("../datasets/gsm8k/train/")
 
 # Loading model
 hf_token = os.getenv("hf_token")
@@ -41,12 +41,12 @@ model.to(device)
 # 8-shot prompt
 fewShotPrompt=f'<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nYou are a helpful assistant<|eot_id|><|start_header_id|>user<|end_header_id|>\n\nBelow are few example question and answer pairs\n\n'
 
-# # 8-shot examples from train set
-# for i in range(9):
-#     fewShotPrompt+=f'Q: {data_train["question"][i]}\n\nA: {data_train["answer"][i]}\n\n'
+# 8-shot examples from train set
+for i in range(9):
+    fewShotPrompt+=f'Q: {data_train["question"][i]}\n\nA: {data_train["answer"][i]}\n\n'
 
 # 8-shot examples from: https://github.com/kojima-takeshi188/zero_shot_cot/blob/main/utils.py
-fewShotPrompt+='Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?\n\nA: There are 15 trees originally.\nThen there were 21 trees after some more were planted.\nSo there must have been 21 - 15 = 6.\n#### 6\n\nQ: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?\n\nA: There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5.\n#### 5\n\nQ: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?\n\nA: Originally, Leah had 32 chocolates.\nHer sister had 42. So in total they had 32 + 42 = 74.\nAfter eating 35, they had 74 - 35 = 39.\n#### 39\n\nQ: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?\n\nA: Jason started with 20 lollipops.\nThen he had 12 after giving some to Denny.\nSo he gave Denny 20 - 12 = 8.\n#### 8\n\nQ: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?\n\nA: Shawn started with 5 toys.\nIf he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9.\n#### 9\n\nQ: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?\n\nA: There were originally 9 computers.\nFor each of 4 days, 5 more computers were added.\nSo 5 * 4 = 20 computers were added.\n9 + 20 is 29.\n#### 29\n\nQ: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?\n\nA: Michael started with 58 golf balls.\nAfter losing 23 on tuesday, he had 58 - 23 = 35.\nAfter losing 2 more, he had 35 - 2 = 33 golf balls.\n#### 33\n\nQ: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?\n\nA: Olivia had 23 dollars.\n5 bagels for 3 dollars each will be 5 x 3 = 15 dollars.\nSo she has 23 - 15 dollars left.\n23 - 15 is 8.\n#### 8\n\n'
+# fewShotPrompt+='Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?\n\nA: There are 15 trees originally.\nThen there were 21 trees after some more were planted.\nSo there must have been 21 - 15 = 6.\n#### 6\n\nQ: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?\n\nA: There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5.\n#### 5\n\nQ: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?\n\nA: Originally, Leah had 32 chocolates.\nHer sister had 42. So in total they had 32 + 42 = 74.\nAfter eating 35, they had 74 - 35 = 39.\n#### 39\n\nQ: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?\n\nA: Jason started with 20 lollipops.\nThen he had 12 after giving some to Denny.\nSo he gave Denny 20 - 12 = 8.\n#### 8\n\nQ: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?\n\nA: Shawn started with 5 toys.\nIf he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9.\n#### 9\n\nQ: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?\n\nA: There were originally 9 computers.\nFor each of 4 days, 5 more computers were added.\nSo 5 * 4 = 20 computers were added.\n9 + 20 is 29.\n#### 29\n\nQ: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?\n\nA: Michael started with 58 golf balls.\nAfter losing 23 on tuesday, he had 58 - 23 = 35.\nAfter losing 2 more, he had 35 - 2 = 33 golf balls.\n#### 33\n\nQ: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?\n\nA: Olivia had 23 dollars.\n5 bagels for 3 dollars each will be 5 x 3 = 15 dollars.\nSo she has 23 - 15 dollars left.\n23 - 15 is 8.\n#### 8\n\n'
 
 model.eval()
 generated_outputs=[]
@@ -68,7 +68,7 @@ for i in tqdm(range(0, len(data_test["question"]), batch_size), desc="Processing
         generated_text = tokenizer.decode(o, skip_special_tokens=True)
         generated_outputs.append({"input": inputs[j], "output": generated_text})
 
-with open("../outputs/gsm8k/LLaMA8B/generated_outputs_test.json", "w") as f:
+with open("../outputs/gsm8k/LLaMA8B/generated_outputs_trainfs_test.json", "w") as f:
     json.dump(generated_outputs, f, indent=4)
 
     
--- kchimmad/score_preds.py
+++ kchimmad/score_preds.py
@@ -2,27 +2,29 @@ import pandas as pd
 from tqdm import tqdm
 from evaluate import load
 import json
-
+from datasets import load_from_disk
 
 def get_answer_dataset(row):
     return row['answer'].split('####')[-1]
 def get_answer_model(row):
     return row['output'].split('####')[-1]
 
-data_feedback = load_from_disk("/scratch/workspace/wenlongzhao_umass_edu-analyze/datasets/gsm8k/feedback")
-model_output_path =  '/scratch/workspace/wenlongzhao_umass_edu-analyze/outputs/gsm8k/LLaMA1B/generated_outputs.json'
-df_model = pd.read_json(model_output_path)
-feedback = pd.DataFrame(data_feedback)
+data_test = load_from_disk("../datasets/gsm8k/test")
+model_output_path =  '../outputs/gsm8k/LLaMA8B/generated_outputs_trainfs_test.json'
+df = pd.read_json(model_output_path)
+test = pd.DataFrame(data_test)
 df['model_answer']=df.apply(get_answer_model,axis=1)
-feedback['GT_Answer']=feedback.apply(get_answer_dataset,axis=1)
-# exact_match_metric = load("exact_match")
+test['GT_Answer']=test.apply(get_answer_dataset,axis=1)
+exact_match_metric = load("exact_match")
 
-# for i in tqdm(range(df.shape[0])):
-#     df['score']=exact_match_metric.compute(predictions=[df.iloc[i]['model_answer']], references=[data_feedback["answer"][i].split("####")[-1]])
+for i in tqdm(range(df.shape[0])):
+    df.loc[i,'score']=exact_match_metric.compute(predictions=[df.iloc[i]['model_answer']], references=[test.iloc[i]["GT_Answer"]])['exact_match']
 
-df['score'] = (df['model_answer'] == feedback['GT_Answer']).astype(int)
+# df['score'] = (df['model_answer'] == feedback['GT_Answer']).astype(int)
+
 df.drop(columns=['model_answer'],inplace=True)
+print("SCORE: ", df['score'].sum()/df.shape[0])
 records = df.to_dict(orient='records')
 formatted_json = json.dumps(records, indent=4)
-with open("/scratch/workspace/wenlongzhao_umass_edu-analyze/Bidirectional-Teacher-Student-Communication-for-LLM-Alignment/outputs/gsm8k/LLaMA8B/generated_outputs1.json", "w") as f:
+with open(model_output_path, "w") as f:
     f.write(formatted_json)
