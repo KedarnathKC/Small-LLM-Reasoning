91457286fe1675f526be8050f192973afd2e2226
KedarnathKC <kedarnathchimmad555@gmail.com>
2025-01-26 03:59:45 +0000

    Added lmeval scripts

---
+++ sbatchScripts/runGranite8B.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export NO_PROGRESS_BAR=true
+export HF_HOME='/scratch/workspace/wenlongzhao_umass_edu-analyze/Bidirectional-Teacher-Student-Communication-for-LLM-Alignment/transformers_cache'
+export HF_TOKEN=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
+hostname
+lm_eval --model hf \
+    --model_args pretrained=ibm-granite/granite-3.0-8b-instruct,token=${HF_TOKEN},cache_dir='/scratch/workspace/wenlongzhao_umass_edu-analyze/Bidirectional-Teacher-Student-Communication-for-LLM-Alignment/transformers_cache' \
+    --tasks gsm8k_cot \
+    --device cuda:0 \
+    --batch_size 32
---
+++ sbatchScripts/runLLaMA3B.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export NO_PROGRESS_BAR=true
+export HF_HOME='/scratch/workspace/wenlongzhao_umass_edu-analyze/Bidirectional-Teacher-Student-Communication-for-LLM-Alignment/transformers_cache'
+export HF_TOKEN=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
+hostname
+lm_eval --model hf \
+    --model_args pretrained=meta-llama/Llama-3.2-3B-Instruct,token=${HF_TOKEN},cache_dir='/scratch/workspace/wenlongzhao_umass_edu-analyze/Bidirectional-Teacher-Student-Communication-for-LLM-Alignment/transformers_cache',dtype='bfloat16' \
+    --tasks gsm8k_cot_llama \
+    --device cuda:0 \
+    --batch_size 64 \
+    --seed 42 \
+    --output_path /scratch/workspace/wenlongzhao_umass_edu-analyze/lm-evaluation-harness/Results/LLaMA1B  \
+    --log_samples \
+    --apply_chat_template  \
+    --fewshot_as_multiturn \
+    --num_fewshot 8 
+    
